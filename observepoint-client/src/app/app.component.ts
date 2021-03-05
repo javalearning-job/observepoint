9a57313dfaa0df02f6d0ab685ab9aa41567e8c68
javalearning-job <79932828+javalearning-job@users.noreply.github.com>
2021-03-05 03:45:09 -0500

    Update app.component.ts

--- observepoint-client/src/app/app.component.ts
+++ observepoint-client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ObserverPoint Employee Management System';
+  title = 'ObservePoint Employee Management System';
 }
